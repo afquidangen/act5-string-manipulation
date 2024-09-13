66d127bb4795f3b4558899c3698e025240acd99c
afquidangen <alyssafayequidangen@gmail.com>
2024-09-13 17:06:06 +0800

    Strings Activity 5

---
+++ strings.js
@@ -0,0 +1,31 @@
+//CONCATENATION
+favActorFirstName = "Rachel";
+favActorLastName = "McAdams";
+fullName = favActorFirstName + " " + favActorLastName;
+console.log(fullName);
+//output: Rachel McAdams
+
+//UPPERCASE AND LOWERCASE
+uppercase = "rachel mcadams";
+console.log(uppercase);
+//output: rachel mcadams
+
+lowercase = "YOSHI";
+console.log(lowercase);
+//output: YOSHI
+
+//APPEND AND CHANGING CASE
+message = "My favorite actor is " + uppercase.toUpperCase() + ".";
+message += " Her best show is The Notebook."
+console.log(message);
+//output: My favorite actor is RACHEL MCADAMS. Her best show is The Notebook.
+
+//overall output: 
+// Rachel McAdams
+// rachel mcadams
+// YOSHI
+// My favorite actor is RACHEL MCADAMS. Her best show is The Notebook.
+
+
+
+
