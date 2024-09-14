8513c70ef483b4b4692e10d7f13cb7bd2041a57b
afquidangen <alyssafayequidangen@gmail.com>
2024-09-14 21:47:00 +0800

    Strings Activity 5

--- strings.js
+++ strings.js
@@ -1,30 +1,51 @@
+//STRINGS MANIPULATION
+
 //CONCATENATION
 favActorFirstName = "Rachel";
 favActorLastName = "McAdams";
 fullName = favActorFirstName + " " + favActorLastName;
-console.log(fullName);
-//output: Rachel McAdams
+console.log("Favorite actor's first name: " + favActorFirstName);
+console.log("Favorite actor's last name: " + favActorLastName);
+console.log("Favorite actor's full name (CONCATENATED): " + fullName);
+//output: 
+// Favorite actor's first name: Rachel
+// Favorite actor's last name: McAdams
+// Favorite actor's full name (CONCATENATED): Rachel McAdams
 
-//UPPERCASE AND LOWERCASE
+//UPPERCASE AND LOWERCASE STRINGS
 uppercase = "rachel mcadams";
-console.log(uppercase);
-//output: rachel mcadams
+console.log("Favorite actor's full name: " + uppercase);
+//output: Favorite actor's full name: rachel mcadams
+
+//CHANGING THE LOWERCASE STRING TO UPPERCASE
+uppercase = "rachel mcadams";
+console.log("Favorite actor's full Name (WHEN CHANGED TO UPPERCASE): " + uppercase.toUpperCase());
+//output: Favorite actor's full name (WHEN CHANGED TO UPPERCASE): RACHEL MCADAMS
+
+//CHANGING THE UPPERCASE STRING TO LOWERCASE
+lowercase = "YOSHI";
+console.log("Favorite pet's name: " + lowercase);
+//output: Favorite pet's name: YOSHI
 
 lowercase = "YOSHI";
-console.log(lowercase);
-//output: YOSHI
+console.log("Favorite pet's name (WHEN CHANGED TO LOWERCASE): " + lowercase.toLowerCase());
+//output: Favorite pet's name (WHEN CHANGED TO LOWERCASE): yoshi
 
 //APPEND AND CHANGING CASE
 message = "My favorite actor is " + uppercase.toUpperCase() + ".";
 message += " Her best show is The Notebook."
-console.log(message);
-//output: My favorite actor is RACHEL MCADAMS. Her best show is The Notebook.
+console.log("APPENDED AND CHANGED CASE: " + message);
+//output: APPENDED AND CHANGED CASE: My favorite actor is RACHEL MCADAMS. Her best show is The Notebook.
 
 //overall output: 
-// Rachel McAdams
-// rachel mcadams
-// YOSHI
-// My favorite actor is RACHEL MCADAMS. Her best show is The Notebook.
+// Favorite actor's first name: Rachel
+// Favorite actor's last name: McAdams
+// Favorite actor's full name (CONCATENATED): Rachel McAdams
+// Favorite actor's full name: rachel mcadams
+// Favorite actor's full name (WHEN CHANGED TO UPPERCASE): RACHEL MCADAMS
+// Favorite pet's name: YOSHI
+// Favorite pet's name (WHEN CHANGED TO LOWERCASE): yoshi
+// APPENDED AND CHANGED CASE: My favorite actor is RACHEL MCADAMS. Her best show is The Notebook.
 
 
 
